e62df7acc83a52c7a5477bb9b43c3497ba60cd05
patschaef <schaefp1@tcnj.edu>
2025-03-31 01:40:21 -0400

    main1 reads file and prints logical addresses
    main1 now reads from file addresses.txt, then stores them in an array of a struct defined by a page number and an offset and prints logical addresses to out1.txt, printed address is bugged it correctly prints the page number and offset, but I believe it should print exactly as read from address.txt

--- main1.c
+++ main1.c
@@ -5,19 +5,73 @@ main1
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+//Definitions
+
+//TODO number of addresses should be discovered at runtime
+//I think we could change it from an array to a linked list
+#define NUM_ADDRESSES 1000
+
+typedef struct LOGICAL_ADDRESS_{
+	unsigned int page_number;
+	unsigned int offset;
+}logical_addy;
+
+//TODO finish implementing TLB
+typedef struct TLB_ENTRY_{
+	unsigned int page_number;
+	unsigned int frame_number;
+}tlb_entry;
+tlb_entry tlb[15];
+
+//Function Declarations
+//takes an address then septerates its pnumber and offset
+void extract(unsigned int address, unsigned int *page_number, unsigned int *offset);
+//Reads and stores from addresses.txt
+void loadaddys(logical_addy * addys);
+//prints logical addresses
+void out1(logical_addy * addys);
+
 
 int main() {
-    unsigned int num = 2; // input decimal address number 
-
-    // Extract higher 8 bits / 2 bytes for page number 
-    // and lower 8 bits / 2 bytes for offset
-    unsigned int page_number = (num >> 8) & 0xFF; // First 8 bits
-    unsigned int offset = num & 0xFF;         // Last 8 bits
+    logical_addy logical_addys[NUM_ADDRESSES];
+	//TODO the size needs to be arbitrary not hardset to 1000
+	loadaddys(logical_addys);
 
     // Output results
-    printf("Page number: 0x%X\n",page_number);
-
-    printf("Offset: 0x%X\n",offset);
-
+	out1(logical_addys);
     return 0;
+}
+
+//Function Definitions
+
+//takes an address then septerates its pnumber and offset
+void extract(unsigned int address, unsigned int *page_number, unsigned int *offset){
+    // Extract higher 8 bits / 2 bytes for page number 
+    // and lower 8 bits / 2 bytes for offset
+    *page_number = (address >> 8) & 0xFF; // First 8 bits
+    *offset = address & 0xFF;         // Last 8 bits
+}
+
+//Reads and stores from addresses.txt
+void loadaddys(logical_addy * addys){
+	FILE * addys_file = fopen("addresses.txt", "r");
+	if(addys_file == NULL){fprintf(stderr, "Error opening addresses.txt\n");exit(1);}
+	for(int i = 0; i<NUM_ADDRESSES; i++){
+		unsigned int curr_address;
+		fscanf(addys_file, "%u\n", &curr_address);
+		extract(curr_address, &addys[i].page_number, &addys[i].offset);
+	}
+	fclose(addys_file);
+}
+
+//prints logical addresses
+void out1(logical_addy * addys){	
+	FILE * out1_file = fopen("out1.txt", "w+");
+	if(out1_file == NULL){fprintf(stderr, "Error opening out1.txt\n");exit(1);}
+	for(int i = 0; i<NUM_ADDRESSES; i++){
+		fprintf(out1_file, "%u%u\n", addys[i].page_number, addys[i].offset);
+	}
+	fclose(out1_file);
 }
